0e6bd42819866bc7901346cfe0fc127b8abf972b
jeremiasianigro <ianigroj@gmail.com>
2025-07-28 15:50:13 -0300

    seccion3-clase7

--- typescript-intro01/src/topics/03-funtions.ts
+++ typescript-intro01/src/topics/03-funtions.ts
@@ -14,11 +14,33 @@ function multiply (firsNumber: number, secondNumber?: number, base: number = 2 )
 }
 
 
-const result: number = addNumber (1,2)
-const result2: string = addNumberArrow (6,9)
-const multiplyResult: number = multiply (5)
+// const result: number = addNumber (1,2)
+// const result2: string = addNumberArrow (6,9)
+// const multiplyResult: number = multiply (5)
+// console.log({result, result2, multiplyResult})
 
 
-console.log({result, result2, multiplyResult})
+interface Character {
+    name: string;
+    hp: number
+    showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number ) => {
+
+    character.hp += amount;
+}
+
+const strider: Character = {
+    name: 'Strider',
+    hp: 50,
+    showHp (){
+        console.log(`Puntos de vida ${this.hp}`)
+    }
+
+}
+
+healCharacter(strider, 50)
+strider.showHp();
 
 export{}
