407cb19b93694316122eff95b0f54d46125d99e2
jeremiasianigro <ianigroj@gmail.com>
2025-07-28 16:40:19 -0300

    seccion3-clase8

--- typescript-intro01/src/main.ts
+++ typescript-intro01/src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 // import './topics/01-basic-type'
 // import './topics/02-objet-interface'
 // import './topics/03-funtions'
-import './topics/04-homeworcks-type'
+// import './topics/04-homeworcks-type'
+import './topics/05-basic-destructuring'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
---
+++ typescript-intro01/src/topics/05-basic-destructuring.ts
@@ -0,0 +1,29 @@
+interface AudioPlayer {
+    audioVolumen: number;
+    songDuration: number;
+    song: string;
+    details: Details ;
+}
+ 
+interface Details {
+    author: string;
+    year: number
+}
+
+const audioPlayer: AudioPlayer = {
+    audioVolumen: 90,
+    songDuration: 36,
+    song: "Mess",
+    details: {
+        author: 'Ed Sheeran',
+        year: 2015
+    }
+}
+
+const {song:anotherSong,details } = audioPlayer
+const {author} = details;
+console.log('Song', anotherSong,'Author', author);
+
+
+
+export{}
