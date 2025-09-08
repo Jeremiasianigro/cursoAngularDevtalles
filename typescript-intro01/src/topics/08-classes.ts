be5ffe28e88d9383ac0e27e739de51fcb5b0d400
jeremiasianigro <ianigroj@gmail.com>
2025-09-08 17:02:37 -0300

    retomamos curso. Clase 15-16

--- typescript-intro01/src/topics/08-classes.ts
+++ typescript-intro01/src/topics/08-classes.ts
@@ -1,4 +1,5 @@
-// ejemplo 1 de class
+
+//* ejemplo 1 de class
 // export class Person {
 //     public name?: string;
 //     private address?: string;
@@ -9,26 +10,55 @@
 //     }
 // }
 
-export class Person {
+ export class Person {
     
     
     constructor (
-        public name: string,
+        public firstName: string,
+        public lastName: string,
         private address:string = 'No address',
     ){}
         
 };
+ //* ejemplo 1 extends
+// export class Hero extends Person {
+//     constructor (
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string,
+//     ){
+//         super(realName, 'San Miguel')
+//     }
+// }
 
-export class Hero extends Person {
+//* ejemplo 2 instanciar un objeto fuera del constructor
+// export class Hero {
+
+//     public person: Person;
+
+//     constructor (
+//         public alterEgo: string,
+//         public age: number,
+//         public realName: string,
+//     ){
+//         this.person = new Person(realName)
+//      }
+// }
+
+//* ejempl 3 instanciar el objeto dentro del constructor
+export class Hero {
+
+    
+
     constructor (
         public alterEgo: string,
         public age: number,
-        public realName: string,
-    ){
-        super(realName, 'San Miguel')
-    }
+        public person: Person,
+
+    ){}
 }
 
-const jeremias = new Hero('Batman', 39, 'Bruse Wayne');
+const Bruce = new Person('Bruce', 'Wayne', 'New York')
+const Batman = new Hero('Batman', 39, Bruce);
 
-console.log(jeremias)
+console.log(Batman);
