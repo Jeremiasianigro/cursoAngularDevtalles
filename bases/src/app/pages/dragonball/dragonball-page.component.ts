ecf36741429530bfa1815a8745c7e7f25bfe4516
Jeremias <ianigro@lacaja.com.ar>
2025-11-20 12:01:20 -0300

    seccion5-clases9-10

--- bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -21,5 +21,22 @@ export class dragonballPageComponent  {
     { id: 3, name: 'Trunks', power: 3000 },
   ]);
 
+  addCharacter() {
+    if (!this.name() || !this.power() || this.power() <= 0) return;
 
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    };
+    // this.characters().push(newCharacter);
+    this.characters.update(characters => [...characters, newCharacter]);
+    this.resetFields();
+
+  }
+
+  resetFields() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
