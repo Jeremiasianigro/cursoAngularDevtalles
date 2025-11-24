aac19cf2ef03174cb06458d44f1e1b8b568bb431
Jeremias <ianigro@lacaja.com.ar>
2025-11-24 17:35:17 -0300

    seccion6-clase8

--- gifs-app/src/app/gifs/components/side-menu-option/side-menu-option.ts
+++ gifs-app/src/app/gifs/components/side-menu-option/side-menu-option.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption{
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-side-menu-option',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-option.html',
 })
-export class SideMenuOption { }
+export class SideMenuOption {
+  menuOption: MenuOption[] = [
+    {
+      icon:'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending',
+    },
+     {
+      icon:'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search',
+    }
+  ];
+
+
+}
