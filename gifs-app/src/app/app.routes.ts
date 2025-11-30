904f567808e526ba479503ff72adf6ce0ee0dd49
Jeremias <ianigro@lacaja.com.ar>
2025-11-30 19:48:25 -0300

    seccion7-clase12

--- gifs-app/src/app/app.routes.ts
+++ gifs-app/src/app/app.routes.ts
@@ -15,6 +15,10 @@ export const routes: Routes = [
           loadComponent: () => import('./gifs/pages/trending-page/trending-page'),
         },
         {
+          path: 'history/:query',
+          loadComponent: () => import('./gifs/pages/gif-history/gif-history'),
+        },
+        {
           path: '**',
           redirectTo: 'trending',
         }
