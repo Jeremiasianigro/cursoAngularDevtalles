18708946619829de7f728057e46cc203c884e536
Jeremias <ianigro@lacaja.com.ar>
2025-12-14 19:35:40 -0300

    seccion12-clase12

--- pipes-app/src/app/pages/uncommon-page/uncommon-page.ts
+++ pipes-app/src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Card } from "../../components/card/card";
+import { I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Jeremias',
+  gender: 'male',
+  age: 39,
+  address: 'Buenos Aires, Argentina'
+};
+
+const client2 = {
+  name: 'Silvana',
+  gender: 'female',
+  age: 41,
+  address: 'San Miguel, Argentina'
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [Card],
+  imports: [Card, I18nSelectPipe],
   templateUrl: './uncommon-page.html',
 })
-export default class UncommonPage { }
+export default class UncommonPage {
+
+  // i18nSelect
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  }
+
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+      this.client.set(client1);
+    }
+
+ }
