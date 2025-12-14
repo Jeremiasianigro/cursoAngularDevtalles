e6cff5df379dca31dc1bec74888858b230b476d3
Jeremias <ianigro@lacaja.com.ar>
2025-12-14 18:22:52 -0300

    seccion12-clase9

--- pipes-app/src/app/app.config.ts
+++ pipes-app/src/app/app.config.ts
@@ -1,12 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEs, 'es');
+registerLocaleData(localeFr, 'fr');
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    },
+
+
   ]
 };
