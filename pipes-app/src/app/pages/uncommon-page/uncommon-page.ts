5191e0e704bd63b75fff74428b6a5846ac63a7fe
Jeremias <ianigro@lacaja.com.ar>
2025-12-14 22:15:52 -0300

    seccion12-clase13

--- pipes-app/src/app/pages/uncommon-page/uncommon-page.ts
+++ pipes-app/src/app/pages/uncommon-page/uncommon-page.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Card } from "../../components/card/card";
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
 
 const client1 = {
   name: 'Jeremias',
@@ -18,10 +18,11 @@ const client2 = {
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [Card, I18nSelectPipe],
+  imports: [Card, I18nSelectPipe, I18nPluralPipe],
   templateUrl: './uncommon-page.html',
 })
 export default class UncommonPage {
+
 
   // i18nSelect
   client = signal(client1);
@@ -40,4 +41,16 @@ export default class UncommonPage {
       this.client.set(client1);
     }
 
+    // i18nPlural
+    clients = signal<string[]>(['Maria', 'Pedro', 'Juan', 'Ana', 'Luis', 'Carlos']);
+    clientsMap = {
+      '=0': 'no tenemos ningún cliente esperando.',
+      '=1': 'tenemos un cliente esperando.',
+      '=2': 'tenemos dos clientes esperando.',
+      other: 'tenemos # clientes esperando.'
+    };
+
+    deleteCliente() {
+      this.clients.update((prev) => prev.slice(1) );
+    }
  }
