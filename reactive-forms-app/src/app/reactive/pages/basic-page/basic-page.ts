fb6dfec29f15fed126441eb260302c098a54876f
Jeremias <ianigro@lacaja.com.ar>
2025-12-20 18:12:20 -0300

    seccion14-clase9

--- reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.ts
+++ reactive-forms-app/src/app/reactive/pages/basic-page/basic-page.ts
@@ -11,7 +11,7 @@ export class BasicPage {
 
 private fb = inject(FormBuilder);
 
-myForm = this.fb.group({
+myForm: FormGroup = this.fb.group({
   name: ['',[Validators.required, Validators.minLength(3)]],
   price: [0, [Validators.required, Validators.min(10)]],
   inStorage: [0, [Validators.required, Validators.min(0)]],
@@ -24,6 +24,31 @@ myForm = this.fb.group({
   //   price: new FormControl(0),
   //   inStorage: new FormControl(0),
   // })
+
+  isValidField(fieldName: string):boolean|null{
+    return !!this.myForm.controls[fieldName].errors;
+  }
+
+  getFiledError(fieldName: string): string|null{
+    if(!this.myForm.controls[fieldName].errors) return null
+
+    const errors = this.myForm.controls[fieldName].errors ?? {};
+
+    for( const key of Object.keys(errors)){
+
+      switch (key){
+        case 'required':
+          return 'Este campo es requerido'
+
+        case 'minlength':
+          return `Minimo de ${ errors['minlength'].requiredLength} caracteres`
+
+        case 'min':
+          return `Valor minimo de ${ errors['min'].min}`
+      }
+    }
+    return null;
+  }
 
 
  }
