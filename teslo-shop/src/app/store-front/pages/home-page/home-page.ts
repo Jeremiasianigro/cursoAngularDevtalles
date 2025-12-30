3ec47532cb2e0e54fa10406681d96926b52d7d5a
Jeremias <ianigro@lacaja.com.ar>
2025-12-30 17:57:56 -0300

    seccion18-clase10

--- teslo-shop/src/app/store-front/pages/home-page/home-page.ts
+++ teslo-shop/src/app/store-front/pages/home-page/home-page.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
-import { ProductCard } from "../../../products/components/product-card/product-card";
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Component, inject } from '@angular/core';
+
+
+
+import { ProductCard } from '@products/components/product-card/product-card';
+import { ProductsService } from '@products/services/product.service';
 
 @Component({
   selector: 'app-home-page',
   imports: [ProductCard],
   templateUrl: './home-page.html',
 })
-export class HomePage { }
+export class HomePage {
+  productsService = inject(ProductsService)
+
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: ({ params }) => this.productsService.getProducts(),
+    });
+ }
