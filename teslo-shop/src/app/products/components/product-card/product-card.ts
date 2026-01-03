25b299d0fc57e8edf7b8395bbd6624ff2246e6aa
Jeremias <ianigro@lacaja.com.ar>
2026-01-03 15:00:07 -0300

    seccion19-clase9

--- teslo-shop/src/app/products/components/product-card/product-card.ts
+++ teslo-shop/src/app/products/components/product-card/product-card.ts
@@ -1,6 +1,7 @@
 import { SlicePipe } from '@angular/common';
 import { Component, computed, input } from '@angular/core';
 import { RouterLink } from "@angular/router";
+
 import { Product } from '@products/interfaces/product.interface';
 import { ProductImagePipe } from '@products/pipes/product-image.pipe';
 
--- teslo-shop/src/app/products/services/product.service.ts
+++ teslo-shop/src/app/products/services/product.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 
 import { Product, ProductsResponse } from '@products/interfaces/product.interface';
 import { environment } from 'src/environments/environment.development';
@@ -16,11 +16,17 @@ interface Options{
 
 @Injectable({providedIn: 'root'})
 export class ProductsService {
+
   private http = inject(HttpClient)
+  private productsCache = new Map<string, ProductsResponse>();
 
   getProducts(options:Options): Observable<ProductsResponse>{
 
     const {limit= 9, offset = 0, gender =''} = options;
+    const key = `${limit}-${offset}-${gender}`
+    if(this.productsCache.has(key)){
+      return of(this.productsCache.get(key)!)
+    }
 
     return this.http
     .get<ProductsResponse>(`${baseUrl}/products`,{
@@ -30,7 +36,10 @@ export class ProductsService {
         gender,
       }
     })
-    .pipe(tap(resp => console.log(resp)));
+    .pipe(
+      tap((resp) => console.log(resp)),
+      tap((resp) => this.productsCache.set(key,resp))
+    );
   }
 
 
