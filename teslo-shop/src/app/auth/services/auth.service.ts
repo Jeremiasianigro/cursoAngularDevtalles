c0ad9280412c34940e85ff3a3e82d7dfb068c78d
Jeremias <ianigro@lacaja.com.ar>
2026-01-04 18:17:07 -0300

    seccion20-clase10

--- teslo-shop/src/app/auth/services/auth.service.ts
+++ teslo-shop/src/app/auth/services/auth.service.ts
@@ -5,6 +5,7 @@ import { User } from '@auth/interfaces/user.interface';
 import { environment } from 'src/environments/environment';
 import { AuthResponse } from '@auth/interfaces/auth-response.interface';
 import { catchError, map, Observable, of, tap } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
 const baseUrl = environment.baseUrl;
@@ -15,6 +16,10 @@ export class AuthService {
   private _token = signal<string | null>(null);
 
   private http = inject(HttpClient)
+
+  checkStatusResource = rxResource({
+    stream: () => this.checkStatus()
+  });
 
 
 
@@ -54,6 +59,36 @@ export class AuthService {
       })
     )
   }
+
+  checkStatus():Observable<boolean>{
+    const token = localStorage.getItem('token');
+    if(!token){
+      return of(false);
+    }
+
+    return this.http.get<AuthResponse>(`${baseUrl}/auth/check-status`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).pipe(
+      tap( resp => {
+        this._user.set(resp.user);
+        this._authStatus.set('authenticated');
+        this._token.set(resp.token);
+
+        localStorage.setItem('token', resp.token);
+      }),
+      map(()=> true),
+      catchError((error: any)=>{
+        this._user.set(null);
+        this._token.set(null);
+        this._authStatus.set('not-authenticated');
+        return of(false);
+      })
+    )
+
+  }
+
 
 
 }
