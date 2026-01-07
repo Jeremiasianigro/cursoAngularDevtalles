5aec6a8676b9b755a20559e7012545b4e9d03b5c
Jeremias <ianigro@lacaja.com.ar>
2026-01-07 15:42:18 -0300

    seccion21-clase8

--- teslo-shop/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
+++ teslo-shop/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+
 import { ProductTable } from "@products/components/product-table/product-table";
-import { ProductImagePipe } from '@products/pipes/product-image.pipe';
+import { ProductsService } from '@products/services/product.service';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
+import { Pagination } from "@shared/components/pagination/pagination";
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [ProductTable],
+  imports: [ProductTable, Pagination],
   templateUrl: './products-admin-page.html',
 })
 export class ProductsAdminPage {
+  productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
 
+  productsPerPage = signal(10);
+
+  productsResource = rxResource({
+    params: () => ({
+      limit: this.productsPerPage(),
+      page: this.paginationService.currentPage() -1}),
+    stream: ({params}) => this.productsService.getProducts({
+      offset: params.page * 9,
+      limit: params.limit
+     }),
+  });
 
 
  }
--- teslo-shop/src/app/products/components/product-table/product-table.ts
+++ teslo-shop/src/app/products/components/product-table/product-table.ts
@@ -1,8 +1,10 @@
 import { CurrencyPipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
 import { Product } from '@products/interfaces/product.interface';
 import { ProductImagePipe } from '@products/pipes/product-image.pipe';
+
 
 @Component({
   selector: 'product-table',
@@ -11,4 +13,5 @@ import { ProductImagePipe } from '@products/pipes/product-image.pipe';
 })
 export class ProductTable {
   products = input.required<Product[]>();
+
 }
