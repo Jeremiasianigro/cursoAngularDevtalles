50b25d650b3a7a652c8a9896e16df64175984d9a
Jeremias <ianigro@lacaja.com.ar>
2026-01-07 16:11:28 -0300

    seccion21-clase9

--- teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-admin-page.ts
+++ teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-admin-page.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs';
+import { ProductsService } from '../../../products/services/product.service';
 
 @Component({
   selector: 'app-product-admin-page',
   imports: [],
   templateUrl: './product-admin-page.html',
 })
-export class ProductAdminPage { }
+export class ProductAdminPage {
+
+  activatedRute = inject(ActivatedRoute);
+  router = inject(Router);
+  ProductService = inject(ProductsService);
+
+  productId = toSignal(
+    this.activatedRute.params.pipe(map((params)=> params['id']))
+  );
+
+  productResource = rxResource({
+    params: () => ({id: this.productId()}),
+    stream: ({params}) => {
+      return this.ProductService.getProductById(params.id)
+    }
+  });
+
+  redirectEffect = effect(()=>{
+    if(this.productResource.error()){
+      this.router.navigate(['/admin/products'])
+    }
+  })
+
+}
