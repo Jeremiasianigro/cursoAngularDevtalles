b734368da778cb380f22a7b49d941f0cac6b43cd
Jeremias <ianigro@lacaja.com.ar>
2026-01-08 13:35:23 -0300

    seccion21-clase11

--- teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
+++ teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -1,21 +1,47 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
+
 import { Product } from '@products/interfaces/product.interface';
 import { ProductCarousel } from "@products/components/product-carousel/product-carousel";
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@utils/form-utils';
 
 @Component({
   selector: 'product-details',
   imports: [ProductCarousel, ReactiveFormsModule],
   templateUrl: './product-details.html',
 })
-export class ProductDetails {
+export class ProductDetails implements OnInit {
   product = input.required<Product>();
 
   fb = inject(FormBuilder);
 
-
-
-
+  productForm = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    slug: ['',
+      [Validators.required, Validators.pattern(FormUtils.slugPattern)]
+    ],
+    price: ['',[Validators.required, Validators.minLength(0)]],
+    stock: ['',[Validators.required, Validators.minLength(0)]],
+    size: [['']],
+    images: [[]],
+    tags: [''],
+    gender: ['',
+      [Validators.required, Validators.pattern(/men|woman|kid|unisex/)]],
+  });
 
   sizes = ['SX','S','M','L','XL','LXX',]
+
+  ngOnInit(): void {
+    this.setFormValue(this.product())
+  }
+
+  setFormValue(formLike: Partial<Product>){
+    this.productForm.reset(this.product() as any)
+    this.productForm.patchValue({tags: formLike.tags?.join(',')})
+  }
+
+  onSubmit(){
+    console.log(this.productForm.value)
+  }
 }
