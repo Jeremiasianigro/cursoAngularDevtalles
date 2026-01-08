d39a0919154d9716596c9eeee2994369a23065de
Jeremias <ianigro@lacaja.com.ar>
2026-01-08 13:24:56 -0300

    seccion21-clase11

--- teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
+++ teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -1,14 +1,21 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { Product } from '@products/interfaces/product.interface';
 import { ProductCarousel } from "@products/components/product-carousel/product-carousel";
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'product-details',
-  imports: [ProductCarousel],
+  imports: [ProductCarousel, ReactiveFormsModule],
   templateUrl: './product-details.html',
 })
 export class ProductDetails {
   product = input.required<Product>();
+
+  fb = inject(FormBuilder);
+
+
+
+
 
   sizes = ['SX','S','M','L','XL','LXX',]
 }
